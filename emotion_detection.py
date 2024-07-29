259de53d323c742f570e5a462d6f884870870492
guifaddoul <guifaddoul@gmail.com>
2024-07-29 17:36:01 -0400

    Commit2

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,7 +7,44 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
 
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-
+          
     response = requests.post(url, json=myobj, headers=header)
 
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    # If the response status code is 200, extract the label and score from the response
+    if response.status_code == 200:
+        anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+        #find the dominant emotion
+        emotion = ['anger','disgust','fear','joy','sadness']
+        score = [anger_score,disgust_score,fear_score,joy_score,sadness_score]
+        max_score = max(score)
+        for i in range(0,5):
+            if max_score == score[i]:
+                dominant_emotion = emotion[i]
+
+    # If the response status code is 500, set label and score to None
+    elif response.status_code == 500:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        dominant_emotion = None
+
+    output = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+            }
+
+    return output
+
