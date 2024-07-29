875c4ae27f5d9a163d522c6c0f5a7fa430ed0f14
guifaddoul <guifaddoul@gmail.com>
2024-07-29 18:59:38 -0400

    Commit5

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detection")
+
+@app.route("/emotionDetector")
+def emotion_analyze():
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    response = emotion_detector(text_to_analyze)
+
+    anger_score = response['anger']
+    disgust_score = response['disgust']
+    fear_score = response['fear']
+    joy_score = response['joy']
+    sadness_score = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    if dominant_emotion is None:
+        return "Invalid input! Try again."
+    else:
+        output = "For the given statement, the system response is 'anger': {anger} 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}.The dominant emotion is {dominant}.".format(anger = anger_score, disgust = disgust_score, fear = fear_score, joy = joy_score, sadness = sadness_score, dominant = dominant_emotion)
+        return output
+        
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
